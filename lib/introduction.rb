178ac2a7ed68b19713bc95b1ab5d43164966d549
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-04 22:30:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,11 @@
 def introduction(name)
+<<<<<<< HEAD
   puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
+=======
+  puts 
+>>>>>>> d6b79cc3d22a8eb126339f0fa2a77e9ba1f3e14f
 end
